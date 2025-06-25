5f574cb20922cfbef3477aef20f893710cdae52f
linhdoan9513 <doanlinh9513@gmail.com>
2025-06-25 15:47:18 -0400

    Add _redirects for SPA routing

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ function OAuth2Callback() {
   return <div>Logging in...</div>;
 }
 
+console.log("🌍 API_BASE from process.env:", process.env.REACT_APP_API_BASE_URL);
+
 function App() {
   return (
     <Router>
